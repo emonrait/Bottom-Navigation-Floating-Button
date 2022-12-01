8b5b99f477c2ac5082498b6376a8293a35b5152a
Raihan Mahamud <48452737+emonrait@users.noreply.github.com>
2022-12-01 17:05:03 +0600

    update

--- app/src/main/java/com/raihan/bottomnavigationflotaingbutton/MainActivity.kt
+++ app/src/main/java/com/raihan/bottomnavigationflotaingbutton/MainActivity.kt
@@ -3,11 +3,15 @@ package com.raihan.bottomnavigationflotaingbutton
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.navigation.NavController
+import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
     private lateinit var bottomNavigationView: BottomNavigationView
+    private lateinit var navController: NavController
+
     private lateinit var fab_QR: FloatingActionButton
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,14 +19,16 @@ class MainActivity : AppCompatActivity() {
         bottomNavigationView = findViewById(R.id.bottomNavigationView)
         fab_QR = findViewById(R.id.fab_QR)
 
+
         bottomNavigationView.background = null
         bottomNavigationView.menu.getItem(2).isEnabled = false
 
         fab_QR.setOnClickListener {
             Toast.makeText(this, "QR Pay Button Test", Toast.LENGTH_SHORT).show()
         }
-
-
+        bottomNavigationView.setupWithNavController(navController)
 
     }
+
+
 }
